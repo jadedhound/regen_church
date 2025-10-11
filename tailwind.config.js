c1e1d07d9864d0e8fb2ffdc5e20655547c82e27a
wilymonkey <codeberg@rohit.top>
2025-10-11 17:37:52 +1100

    rm formatters

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./templates/**/*.html", "./theme/**/*.html", "./content/**/*.md"],
-  plugins: [
-    require("@tailwindcss/typography"),
-    require("postcss-nested"),
-  ],
-};
